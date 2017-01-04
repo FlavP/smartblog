82f4edf74a3a63202ce8611b50a078dc516f4bd7
flavius <flavius.popoiu@mediaimage.ro>
2017-01-04 14:05:53 +0200

    configured postgres connectivity

--- smartblog/settings.py
+++ smartblog/settings.py
@@ -73,13 +73,24 @@ WSGI_APPLICATION = 'smartblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'smartblog', # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': 'purrgres',
+        'PASSWORD': 'BunaZiua1234',
+        'HOST': '127.0.0.1', # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
