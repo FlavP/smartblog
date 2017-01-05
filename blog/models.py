5d286c80957ba675790004e1d3472f9369216d7f
flavius <flavius.popoiu@mediaimage.ro>
2017-01-05 17:53:44 +0200

    more model work

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,34 @@
 from django.db import models
+from centralizer.models import Tag, Company
 import datetime
 # Create your models here.
 
 class Article(models.Model):
-    articleid = models.AutoField(primary_key=True)
+    articleid = models.AutoField(
+        primary_key=True,
+        serialize=False,
+        verbose_name='articleid',
+        auto_created=True
+    )
     title = models.CharField(max_length=63)
-    art_slug = models.SlugField()
+    #we check for uniqueness for articles published in the same monts
+    art_slug = models.SlugField(
+                                max_length=63,
+                                help_text="Label for URL composition",
+                                unique_for_month='pub_date',
+                                )
     content = models.TextField()
-    pub_date = models.DateField()
-    added = models.DateField(default = datetime.now, blank = False)
+    added = models.DateField('date when the article was added', auto_now_add=True)
+    tags = models.ManyToManyField(Tag, related_name='articles')
+    companies = models.ManyToManyField(Company, related_name='articles')
 
+    def __str__(self):
+        return "{} on {}".format(
+            self.title,
+            self.added.strftime("%Y-%m-%d")
+        )
 
+    class Meta:
+        verbose_name = "blog article"
+        ordering = ["-added", "title"]
+        get_latest_by = 'added'
--- centralizer/models.py
+++ centralizer/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+class Tag(models.Model):
+    tagid = models.AutoField(
+        primary_key=True,
+        serialize=False,
+        verbose_name='tagid',
+        auto_created=True
+    )
+    tagname = models.CharField(max_length=31, unique=True)
+    tag_slug = models.SlugField()
+    added = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.tagname
+
+    class Meta:
+        ordering = ['tagname']
+
+class Company(models.Model):
+    companyid = models.AutoField(
+        primary_key=True,
+        serialize=False,
+        verbose_name='compid',
+        auto_created=True
+    )
+    company_name = models.CharField(max_length=31)
+    company_slug = models.SlugField(max_length=31, unique=True, help_text="Label for URL composition")
+    description = models.TextField()
+    founded_date = models.DateField('date when it was founded')
+    email = models.EmailField()
+    website = models.URLField(max_length=255)
+    added = models.DateField(auto_now_add=True)
+    # It is entirely up to you where you define the many to many relationship, related_type --> the name
+    # to refer from the other table to this one, like t.companies (t short for tags) or c.tags (c short for company)
+    tags = models.ManyToManyField(Tag, related_name='companies')
+
+    def __str__(self):
+        return self.company_name
+
+    class Meta:
+        ordering = ['company_name']
+        get_latest_by = 'founded_date'
+
+class RelatedNews(models.Model):
+    title = models.CharField(max_length=63)
+    pub_date = models.DateField('date published')
+    newslink = models.URLField(max_length=255)
+    company = models.ForeignKey(Company)
+
+    def __str__(self):
+        #self.company calls the str (string) method of the company object
+        return "{}:{}",format(self.company, self.title)
+
+    class Meta:
+        #verbose = change the display string of a class
+        verbose_name = "related news"
+        ordering = ['-pub_date']
+        get_latest_by = 'pub_date'
