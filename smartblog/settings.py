828b9ea1febdb15e235ec9aee4f0eaed87993a0c
FlavP <flaviuspopoiu@gmail.com>
2017-01-28 01:35:40 +0200

    601.1

--- smartblog/settings.py
+++ smartblog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+from django.conf.global_settings import EMAIL_BACKEND
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'centralizer',
     'blog',
+    'contact',
 ]
 
 MIDDLEWARE = [
@@ -133,3 +135,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Email
+# https://docs.djangoproject.com/en/1.10/topics/email
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
