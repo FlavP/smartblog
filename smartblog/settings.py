a266ee9fac104a679ca729634e8da02bc57afb6d
FlavP <flaviuspopoiu@gmail.com>
2017-02-01 00:44:16 +0200

    732

--- smartblog/settings.py
+++ smartblog/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 from django.conf.global_settings import EMAIL_BACKEND, SERVER_EMAIL,\
-    DEFAULT_FROM_EMAIL, EMAIL_SUBJECT_PREFIX, MANAGERS
+    DEFAULT_FROM_EMAIL, EMAIL_SUBJECT_PREFIX, MANAGERS, STATICFILES_DIRS
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -141,7 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 # Email
 # https://docs.djangoproject.com/en/1.10/topics/email
 
