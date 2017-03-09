b4c2bc82d3d947b7e9838d905b7b0edd5369d5f9
flavius <flavius.popoiu@mediaimage.ro>
2017-03-09 17:01:14 +0200

    doar putin mai in fata

--- user/utils.py
+++ user/utils.py
@@ -1,7 +1,12 @@
+from smtplib import SMTPException
+
 from django.template.loader import render_to_string
 from django.contrib.auth.tokens import default_token_generator as toka_toka
+from django.contrib.sites.shortcuts import get_current_site
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
+from django.conf import settings
+from django.core.mail import (BadHeaderError, send_mail)
 
 class ActivationMailFormMixin:
     mail_validation_error = ''
@@ -36,3 +41,40 @@ class ActivationMailFormMixin:
     def get_context_data(self, request, user, context=None):
         if context is None:
             context = dict()
+        current = get_current_site(request)
+        if request.is_secure():
+            protocol = 'https'
+        else:
+            protocol = 'http'
+            token = toka_toka.make_token(user)
+            uid = urlsafe_base64_encode(force_bytes(user.pk))
+        context.update({
+            'domain': current.domain,
+            'protocol': protocol,
+            'site_name': current.name,
+            'token': token,
+            'user': user,
+            'uid': uid,
+        })
+        return context
+
+    def _send_mail(self, request, user, **kwargs):
+        kwargs['context'] = self.get_context_data(request, user)
+        mail_kwargs = {
+            'subject': self.get_subject(**kwargs),
+            'message': self.get_message(**kwargs),
+            'from_email': (settings.DEFAULT_FROMEMAIL),
+            'recipient_list': [user.email],
+        }
+        try:
+            no_send = send_mail(**mail_kwargs)
+        except Exception as err:
+            self.log_email_error(error=err, **mail_kwargs)
+            if isinstance(err, BadHeaderError):
+                err_type = 'badheader'
+            if isinstance(err, SMTPException):
+                err_type = 'smtperror'
+            else:
+                err_type = 'unknow'
+            return (False, err_type)
+
